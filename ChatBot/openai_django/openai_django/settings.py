fe5e6d18259d30a2c37ad81d90c44066ffb36e05
Christos218 <131569739+Christos218@users.noreply.github.com>
2025-08-02 08:24:02 +0300

    Update settings.py

--- ChatBot/openai_django/openai_django/settings.py
+++ ChatBot/openai_django/openai_django/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = 'PUT YOU OPEN AI KEY'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
